0614a4190fd3b9e8d4ed34f66d2298d1c1f4a637
KiraArensky <kshi.txt@yandex.ru>
2024-02-28 11:51:34 +0500

    v1.1 (bugfix, need pymorphy)

--- script/mute.py
+++ script/mute.py
@@ -1,17 +1,11 @@
-import pymorphy2
 import time
 
 
 def user_mute(message, bot):
-    morph = pymorphy2.MorphAnalyzer()
-
     try:
         mute_time = int(message.text[6:]) * 60
     except ValueError:
         mute_time = 60
-
-    comment = morph.parse('минуту')[0]
-    msg_text = comment.make_agree_with_number(int(mute_time / 60)).word
 
     bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAIC-mLa6-18z-1E9KJEj45-jh-yDa24AAIeGgACPDxxSo3gZaQZq5ewKQQ")
     try:
@@ -19,7 +13,7 @@ def user_mute(message, bot):
                                  until_date=time.time() + mute_time)
         bot.send_message(message.chat.id,
                          f'[Чурка](tg://user?id={message.reply_to_message.from_user.id}) ты в муте на'
-                         f' {int(mute_time / 60)} {msg_text}',
+                         f' {int(mute_time / 60)} минут',
                          disable_web_page_preview=True, parse_mode="Markdown")
     except Exception as e:
         bot.send_message(message.chat.id,
